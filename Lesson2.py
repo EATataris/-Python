482c0db2bbaf8316732a98036886c8d3920f7fb1
Sasuke <emir-ali818@mail.ru>
2020-09-30 21:04:37 +0300

    Дз к уроку номер 2

--- Lesson2.py
+++ Lesson2.py
@@ -1,7 +1,7 @@
 #1
-# default_list = [1, 'Hello', None, True, [2, 3], {'x': 1, 'y': 2}]
-# for i in default_list:
-#     print(f'{i} - {type(i)}')
+default_list = [1, 'Hello', None, True, [2, 3], {'x': 1, 'y': 2}]
+for i in default_list:
+    print(f'{i} - {type(i)}')
 
 #2
 def split_list(list):
@@ -21,3 +21,48 @@ def split_list(list):
 
 ls = [1, 2, 3, 4, 5]
 print(split_list(ls))
+
+#3
+#решение списками
+winter = [1, 2, 12]
+spring = [3, 4, 5]
+summer = [6, 7, 8]
+autumn = [9, 10, 11]
+try:
+    month = int(input('Введите номер месяца: '))
+    if month in winter:
+        print('Зима')
+    elif month in spring:
+        print('Весна')
+    elif month in summer:
+        print('Лето')
+    elif month in autumn:
+        print('Осень')
+    else:
+        print('Такого месяца не существует')
+except ValueError:
+    print('Введите целое число')
+
+#решение словарем
+year = {'winter': [1, 2, 12],
+        'spring': [3, 4, 5],
+        'summer': [6, 7, 8],
+        'autumn': [9, 10, 11]}
+
+month = int(input('Введите номер месяца: '))
+for k, v in year.items():
+    if month in v:
+        print(k)
+
+#4
+string = input('Введите произвольную строку: ')
+string_list = string.split()
+for i, j in enumerate(string_list, 1):
+    print(i, j[:10])
+
+#5
+my_list = [7, 5, 3, 3, 2]
+rating = int(input('Веедите новый элемент рейтинга: '))
+my_list.append(rating)
+sorted_list = [str(i) for i in sorted(my_list, reverse=True)]
+print(', '.join(sorted_list))
